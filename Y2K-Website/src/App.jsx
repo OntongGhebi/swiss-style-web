d48d95e18d4f013800443a1a15b278784f47e306
OntongGhebi <ghebiarmando@gmail.com>
2025-12-02 00:23:18 +0700

    progress FullScreenNavbar 40%

--- Y2K-Website/src/App.jsx
+++ Y2K-Website/src/App.jsx
@@ -3,16 +3,18 @@ import Home from "./pages/Home";
 import Agence from "./pages/Agence";
 import Projects from "./pages/Projects";
 import Navbar from "./components/navigation/Navbar";
+import FullScreenNav from "./components/navigation/FullScreenNav";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Routes>
+      {/* <Navbar /> */}
+      <FullScreenNav />
+      {/* <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/agence" element={<Agence />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
-      </Routes>
+      </Routes> */}
     </div>
   );
 };
