1aefb025832b71ea817830f2e954b6deef6153a5
OntongGhebi <ghebiarmando@gmail.com>
2025-12-04 23:52:33 +0700

    Finished for ProjectCard

---
+++ Y2K-Website/src/components/projects/ProjectCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ProjectCard = (props) => {
+  return (
+    <div className="w-full h-[500px] flex mb-4 gap-4">
+      <div className="w-1/2 group transition-all relative rounded-none hover:rounded-[50px] overflow-hidden h-full">
+        <img className="h-full w-full object-cover" src={props.image1} alt="" />
+        <div className="opacity-0 transition-all group-hover:opacity-100 absolute top-0 flex items-center justify-center left-0 h-full w-full bg-black/10">
+          <h2 className="uppercase text-6xl font-[font2] border-2 border-white pt-4 px-4 text-white rounded-full">
+            Voir le Project
+          </h2>
+        </div>
+      </div>
+      <div className="w-1/2 group transition-all relative rounded-none hover:rounded-[70px] overflow-hidden h-full">
+        <img className="h-full w-full object-cover" src={props.image2} alt="" />
+        <div className="opacity-0 transition-all group-hover:opacity-100 absolute top-0 flex items-center justify-center left-0 h-full w-full bg-black/10">
+          <h2 className="uppercase text-6xl font-[font2] border-2 border-white pt-4 px-4 text-white rounded-full">
+            Voir le Project
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- Y2K-Website/src/pages/Projects.jsx
+++ Y2K-Website/src/pages/Projects.jsx
@@ -1,27 +1,27 @@
 import React from "react";
+import ProjectCard from "../components/projects/ProjectCard";
 
 const Projects = () => {
-  // const projects = [
-  //   {
-  //     image1:
-  //       "https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg",
-  //       "https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg",
-  //     image2:
-  //       "https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg",
-  //   },
-  //   {
-  //     image1:
-  //       "https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg",
-  //     image2:
-  //       "https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg",
-  //   },
-  //   {
-  //     image1:
-  //       "https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg",
-  //     image2:
-  //       "https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg",
-  //   },
-  // ];
+  const projects = [
+    {
+      image1:
+        "https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg",
+      image2:
+        "https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg",
+    },
+    {
+      image1:
+        "https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg",
+      image2:
+        "https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg",
+    },
+    {
+      image1:
+        "https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg",
+      image2:
+        "https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg",
+    },
+  ];
 
   return (
     <div className="p-4">
@@ -29,29 +29,9 @@ const Projects = () => {
         <h2 className="font-[font2] text-[23vh] uppercase">Projects</h2>
       </div>
       <div className="-mt-15">
-        <div className="w-full h-[500px] flex mb-4 gap-4">
-          <div className="w-1/2 transition-all rounded-none hover:rounded-[40px] overflow-hidden h-full">
-            <img
-              className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
-              alt=""
-            />
-            <div>
-              <h2></h2>
-            </div>
-          </div>
-          {/* <div className="w-1/2 hover:rounded-4xl overflow-hidden h-full">
-            <img
-              className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg"
-              alt=""
-            />
-          </div> */}
-        </div>
-        {/* <div className="w-full h-[700px] flex mb-4 gap-4 bg-amber-600 -mt-15">
-          <div className="w-1/2 h-full bg-green-500"></div>
-          <div className="w-1/2 h-full bg-blue-500"></div>
-        </div> */}
+        {projects.map(function (elem) {
+          return <ProjectCard image1={elem.image1} image2={elem.image2} />;
+        })}
       </div>
     </div>
   );
